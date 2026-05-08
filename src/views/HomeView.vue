3f751212da37eecb26fb8285cd7411fda7c8d285
Kevin Steve Sathyanath <satke569@student.otago.ac.nz>
2026-05-08 22:55:33 +1200

    Removed all bloat from Vue
    - Now at a nice start state for development.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,22 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div>
+
+    </div>
   </main>
 </template>
+
+
+<style>
+
+main {
+  background-color: var(--background-colour);
+  min-height: 100vh;
+}
+
+
+</style>
