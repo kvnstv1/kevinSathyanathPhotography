de82b6405158ee654352248950c406fb4083aac2
Kevin Steve Sathyanath <satke569@student.otago.ac.nz>
2026-05-13 19:18:09 +1200

    Basic homepage added
    - Homepage colopur scheme looks good for now
    - Other structure is missing
    - Need to make backend to pull data from on AWS

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-
+<RouterView />
 <div></div>
 </template>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
-
+import '../assets/style.css'
 </script>
 
 <template>
   <main>
-    <div>
-
+    <div class="full-page">
+      <div class="content-holder">
+        <div class="title">
+          Kevin Steve Sathyanath
+        </div>
+      </div>
     </div>
   </main>
 </template>
@@ -13,10 +17,36 @@
 
 <style>
 
-main {
+@import url('https://fonts.googleapis.com/css2?family=Corben:wght@400;700&display=swap');
+
+.full-page {
   background-color: var(--background-colour);
-  min-height: 100vh;
+  color: var(--headings-colour);
+  height:100vh;
+  display: flex;
 }
 
+.content-holder {
+  width: 100%;
+  margin: 2rem;
+}
 
+.title{
+  font-family: "Corben", serif;
+  font-weight: 900;
+  font-style: normal;
+  font-size: xx-large
+}
+
+.corben-regular {
+  font-family: "Corben", serif;
+  font-weight: 400;
+  font-style: normal;
+}
+
+.corben-bold {
+  font-family: "Corben", serif;
+  font-weight: 700;
+  font-style: normal;
+}
 </style>
